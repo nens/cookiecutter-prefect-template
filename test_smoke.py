ac77a5e509b00fa9144f76192c33b030f8cd68d3
Reinout van Rees <reinout@vanrees.org>
2024-05-08 16:12:12 +0200

    Add test to lint+test the generated code

--- test_smoke.py
+++ test_smoke.py
@@ -1,4 +1,7 @@
+import sys
+from contextlib import chdir
 from pathlib import Path
+from subprocess import run
 
 from cookiecutter.main import cookiecutter
 
@@ -10,3 +13,20 @@ def test_smoke(tmp_path: Path):
         extra_context={"project_name": "my-example", "project_number": "R1972"},
         no_input=True,
     )
+
+
+# TODO: skip this test on windows as it won't work.
+def test_generated_project(tmp_path: Path):
+    cookiecutter(
+        template=".",
+        output_dir=str(tmp_path),
+        extra_context={"project_name": "my-example", "project_number": "R1972"},
+        no_input=True,
+    )
+    with chdir(tmp_path / "my-example"):
+        run([sys.executable, "-m", "venv", ".venv"], check=True)
+        run([".venv/bin/pip", "install", "-r", "requirements.txt"], check=True)
+        run([".venv/bin/pip", "install", "pre-commit"], check=True)
+        run(["git", "init"], check=True)
+        run([".venv/bin/pre-commit", "run", "--all"], check=True)
+        run([".venv/bin/pytest"], check=True)
