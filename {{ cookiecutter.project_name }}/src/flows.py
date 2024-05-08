0d02a5375bb797ee9bfc6fd8428dc28a6422ee51
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-05-08 14:49:27 +0200

    changed example_flow to main -> prevents having to change it for each flow

--- {{ cookiecutter.project_name }}/src/flows.py
+++ {{ cookiecutter.project_name }}/src/flows.py
@@ -8,11 +8,11 @@ def uppercase_the_text(text: str) -> str:
 
 
 @flow(log_prints=True)
-def example_flow(text: str = "Hi"):
+def main(text: str = "Hi"):
     uppercase_text = uppercase_the_text(text)
     print(f"Turned {text} into {uppercase_text}")
 
 
 if __name__ == "__main__":
     print("Not serving flows, but running them locally for testing")
-    example_flow()
+    main()
