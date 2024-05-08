01931261d7f379d2c24f21cec56460d5e738f109
Reinout van Rees <reinout@vanrees.org>
2024-05-08 17:08:52 +0200

    Added test that fails: we want to strip off prefect- from the prefix

--- test_smoke.py
+++ test_smoke.py
@@ -16,6 +16,21 @@ def test_smoke(tmp_path: Path):
     )
 
 
+def test_function_prefix(tmp_path: Path):
+    cookiecutter(
+        template=".",
+        output_dir=str(tmp_path),
+        extra_context={
+            "project_name": "prefect-nens-customer",
+            "project_number": "R1972",
+        },
+        no_input=True,
+    )
+    generated_flows_py = tmp_path / "prefect-nens-customer/src/flows.py"
+    # We prefix everything with our project name, but want to omit the prefect part.
+    assert "def nens_customer_example_flow" in generated_flows_py.read_text()
+
+
 def test_generated_project(tmp_path: Path):
     if sys.platform.startswith("win"):
         pytest.skip("Skipping test that uses linux commands")
