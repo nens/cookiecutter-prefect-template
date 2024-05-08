a43b52b2121fbfaf00dc8e042c880940914d5b23
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-05-08 14:53:32 +0200

    loaded api url from dotenv

--- {{ cookiecutter.project_name }}/src/server.py
+++ {{ cookiecutter.project_name }}/src/server.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
 
-from flows import example_flow
+from flows import main
 from prefect import serve
+
+load_dotenv()
 
 if __name__ == "__main__":
     assert "PREFECT_API_URL" in os.environ
     print(f"Serving flows for {os.environ['PREFECT_API_URL']}")
 
-    deploy1 = example_flow.to_deployment(
+    deploy1 = main.to_deployment(
         name="test-deploy",
         interval=60,
         parameters={"text": "Python is absolutely fabulous"},
