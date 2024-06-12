391e87a23f88f76607f37530c272162f6695a8fa
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-06-12 14:00:07 +0200

    added default description

--- {{ cookiecutter.project_name }}/src/server.py
+++ {{ cookiecutter.project_name }}/src/server.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         name="Clear name of your deployment",
         interval=60, # alternative: cron="5 4 * * *". Check crontab.guru to create the correct schedule expression
         parameters={"text": "Python is absolutely fabulous"}, # Input needs to be convertable to json
-        description="Place a clear description here",
+        description="Code found at: https://github.com/nens/{{ project_name }}", # Place this in EACH deployment, so that it its clear which repo serves which deployment
         tags=["Your name", "Project keyword"] # Add the name of the first point of contact, and one or more keywords of the project
     )
 
