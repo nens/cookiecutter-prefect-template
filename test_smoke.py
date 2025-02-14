06980744c496adb484c9f54d109be48a2472c441
Reinout van Rees <reinout@vanrees.org>
2025-02-14 15:18:49 +0100

    Separated out various stages (and removed pytest running...)

--- test_smoke.py
+++ test_smoke.py
@@ -32,7 +32,20 @@ def test_function_prefix(tmp_path: Path):
     assert "def nens_customer_flow" in generated_flows_py.read_text()
 
 
-def test_generated_project(tmp_path: Path):
+def test_generated_project_ruff(tmp_path: Path):
+    if sys.platform.startswith("win"):
+        pytest.skip("Skipping test that uses linux commands")
+    cookiecutter(
+        template=".",
+        output_dir=str(tmp_path),
+        extra_context={"project_name": "prefect-my-example", "project_number": "R1972"},
+        no_input=True,
+    )
+    with chdir(tmp_path / "prefect-my-example"):
+        run([sys.executable, "-m", "ruff", "format"], check=True)
+
+
+def test_generated_project_precommit(tmp_path: Path):
     if sys.platform.startswith("win"):
         pytest.skip("Skipping test that uses linux commands")
     cookiecutter(
@@ -44,8 +57,18 @@ def test_generated_project(tmp_path: Path):
     with chdir(tmp_path / "prefect-my-example"):
         run(["git", "init"], check=True)
         run(["git", "add", "-A"], check=True)
-        run([sys.executable, "-m", "ruff", "format"], check=True)
+        run([sys.executable, "-m", "pre_commit", "run", "--all"], check=True)
+
+
+def test_generated_project_install(tmp_path: Path):
+    if sys.platform.startswith("win"):
+        pytest.skip("Skipping test that uses linux commands")
+    cookiecutter(
+        template=".",
+        output_dir=str(tmp_path),
+        extra_context={"project_name": "prefect-my-example", "project_number": "R1972"},
+        no_input=True,
+    )
+    with chdir(tmp_path / "prefect-my-example"):
         run([sys.executable, "-m", "venv", ".venv"], check=True)
         run([".venv/bin/pip", "install", "-r", "requirements.txt"], check=True)
-        run([sys.executable, "-m", "pre_commit", "run", "--all"], check=True)
-        run([".venv/bin/pytest"], check=True)
