b4d15e6ac5a481cf897168deea2c0a1a88419859
Reinout van Rees <reinout@vanrees.org>
2025-02-19 12:09:16 +0100

    Added debug instructions to server.py

--- {{ cookiecutter.project_name }}/src/server.py
+++ {{ cookiecutter.project_name }}/src/server.py
@@ -3,6 +3,11 @@ from prefect import serve
 from flows import {{ cookiecutter.__underscore_name }}_flow
 
 if __name__ == "__main__":
+    # This __main__ is called in the dockerfile to actually serve your flows. You can
+    # test the setup here by running prefect locally and calling this file with the
+    # PREFECT_API_URL environment variable set to your locally running prefect.
+    # The main testing/tweaking during development will be in flows.py and tasks.py.
+
     {{ cookiecutter.__underscore_name }}_deployment = {{ cookiecutter.__underscore_name }}_flow.to_deployment(
         name="Clear name of your deployment",
         interval=60,
@@ -17,4 +22,9 @@ if __name__ == "__main__":
     )
 
     serve({{ cookiecutter.__underscore_name }}_deployment)
-    # It is possible to create multiple deployments and serve them all.
+    # It is possible to create multiple deployments and serve them all, just pass them like:
+    # serve(
+    #     {{ cookiecutter.__underscore_name }}_deployment1,
+    #     {{ cookiecutter.__underscore_name }}_deployment2,
+    #     {{ cookiecutter.__underscore_name }}_deployment3
+    # )
