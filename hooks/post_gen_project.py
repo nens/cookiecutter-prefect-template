a8d9fefefb3310c1f88e5a37b051aba29d31919a
Reinout van Rees <reinout@vanrees.org>
2025-02-19 10:29:28 +0100

    Even nicer formatting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,17 +21,17 @@ def print_instructions(
     print("")
     print("Hurray!!!")
     print("")
-    print("First, use the following url to a new empty repo on github.")
-    print("'Empty' means don't let github generate a readme, .gitignore or license:")
-    print("the cookiecutter already has them ready for you.")
+    print("")
+    print("→ First, use the following url to a new empty repo on github.")
+    print("  'Empty' means don't let github generate a readme, .gitignore or license,")
+    print("  the cookiecutter already has them ready for you.")
     print("")
     print(creation_url)
     print("")
-    print("Done?")
-    print("")
-    print("Then go to the generated project and do some git magic:")
     print("")
 
+    print("→ Done? Go to your generated project and do some git magic:")
+    print("")
     print(f"cd {current_dir}")
     print("git init")
     print("git add -A")
@@ -39,7 +39,16 @@ def print_instructions(
     print("git branch -M main")
     print(f"git remote add origin git@github.com:nens/{project_name}.git")
     print("git push origin")
-    print(" ")
+    print("")
+    print("")
+
+    readme_url = (
+        "https://github.com/nens/{{ cookiecutter.project_name }}/blob/main/README.md"
+    )
+    print("→ Lastly, start on the helpful checklist in the readme:")
+    print("")
+    print(readme_url)
+    print("")
 
 
 if __name__ == "__main__":
